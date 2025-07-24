071669326609d9334180b5fa340c3bf745c288f0
AntonniSMoraes <antonnimoraes@gmail.com>
2025-07-24 00:43:01 -0300

    footer visual done

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,7 @@
 export const logo = "https://res.cloudinary.com/dzok2qpsz/image/upload/v1753323721/logo_zcbtkj.svg"
 
+export const logo_branco = "https://res.cloudinary.com/dzok2qpsz/image/upload/v1753327193/Frame_37_vui2mu.svg"
+
 export const frame1 = "https://res.cloudinary.com/dzok2qpsz/image/upload/v1753323720/frame1_vxahkn.jpg"
 
 export const frame2 = "https://res.cloudinary.com/dzok2qpsz/image/upload/v1753323725/frame2_ydotrt.jpg"
@@ -9,3 +11,9 @@ export const frame3 = "https://res.cloudinary.com/dzok2qpsz/image/upload/v175332
 export const search = "https://res.cloudinary.com/dzok2qpsz/image/upload/v1753323721/search_jyfyjy.svg"
 
 export const bag = "https://res.cloudinary.com/dzok2qpsz/image/upload/v1753323721/Bag_pguviv.svg"
+
+export const facebook_logo = "https://res.cloudinary.com/dzok2qpsz/image/upload/v1753323770/facebook_icon_p0sp2l.svg"
+
+export const twitter_logo = "https://res.cloudinary.com/dzok2qpsz/image/upload/v1753323771/twitter_icon_f8fhj4.svg"
+
+export const instagram_logo = "https://res.cloudinary.com/dzok2qpsz/image/upload/v1753323770/instagram_icon_vjnxd2.svg"
